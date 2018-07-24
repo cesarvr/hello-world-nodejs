b4be8f033c0cc003a28bbf526e8bfd4024d7252e
cesar <cvaldezr@redhat.com>
2018-07-24 10:49:50 +0200

    initial

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+let count = 0
+let status = 200
+const msg = {
+    200: 'Hello World!!',
+    500: 'Boom!'
+}
+
+const random = Math.floor(Math.random() * 10) + 1  
+
 require('http').createServer((req, res) => {
-    res.end('Hello World')
+    res.writeHead(status, { 'Content-Type': 'text/plain' })
+
+    res.end('> '+ msg[status])
+
+    if (count > random)
+        status = 500
+
+    count++
 }).listen(8080)
