aa2ca23c3078b8aa01f4dfe31d9214718def5371
cesar <cesarv01@gmail.com>
2018-08-14 15:56:39 +0100

    adding fib5

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+function fibonacci(num) {
+  if (num <= 1) return 1;
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+let rnd = () => Math.floor(Math.random() * 6) + 1   
+
 require('http').createServer((req, res) => {
-    res.end('Hello World')
+    
+    res.end( 'Hello World V1', fibonacci(rnd(5)) )
 }).listen(8080)
