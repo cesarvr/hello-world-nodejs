9d5b5bad2efdbddd15f04d922ceef646036594e7
cesar <cesarv01@gmail.com>
2018-08-15 08:07:24 +0100

    fix

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-function fibonacci(num) {
-  if (num <= 1) return 1;
-
-  return fibonacci(num - 1) + fibonacci(num - 2);
-}
-
-const N = 50
-
 require('http').createServer((req, res) => {
- console.log('running fib: ', N) 
- res.end( 'Hello World V1' +  fibonacci(N) )
+ res.end( 'Hello World V1')
 }).listen(8080)
