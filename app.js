f0e0c70939808046bfeacebe3d066b6e4432ea2b
cesar <cesarv01@gmail.com>
2018-08-16 16:07:55 +0100

    adding stuff

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-function fibonacci(num) {
-  if (num <= 1) return 1;
+const url = require('url')
 
-  return fibonacci(num - 1) + fibonacci(num - 2);
+function fibonacci(num) {
+  if (num <= 1) return 1
+
+  return fibonacci(num - 1) + fibonacci(num - 2)
+}
+
+function get_query(request) {
+  var url_parts = url.parse(request.url, true)
+  return url_parts.query
 }
 
 const N = 5
 
 require('http').createServer((req, res) => {
- console.log('running fib: ', N) 
- res.end( 'fibonacci: iter:'+N+ "->" +  fibonacci(N) )
+ const query = get_query(req) 
+ res.end( 'fibonacci: iter:'+N+ "->" +  fibonacci(N) + ' query->' + query )
 }).listen(8080)
