5c363b8467af42575dfb9cad95742a065ac5e442
cesar <cesarv01@gmail.com>
2018-08-16 16:21:18 +0100

    - updating app: 16:21:18 date: 16-08-2018

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 git add .
-git commit -m " - updating blog  time: $(date +"%T") date: $(date +"%d-%m-%Y")"
-git push origin master
+git commit -m " - updating app: $(date +"%T") date: $(date +"%d-%m-%Y")"
+git push origin fibonacci
